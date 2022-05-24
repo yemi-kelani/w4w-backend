4417712e5798b585163afba79c4dafbfcc1f1e0e
Yemi Kelani <91276489+yemi-kelani@users.noreply.github.com>
2022-05-23 21:48:37 -0500

    server created

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+from dotenv import load_dotenv
+import requests
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+
+@app.route('/wakeup')
+def wake_up():
+    return 'Backend is awake!'
+
+@app.route('/besttime', methods=["POST"])
+def besttime():
+    BESTTIME_PRI_APIKEY = os.getenv('BESTTIME_PRI_APIKEY')
+    input_json = request.get_json(force=True) 
+    url = "https://besttime.app/api/v1/forecasts"
+
+    params = {
+        'api_key_private': BESTTIME_PRI_APIKEY,
+        'venue_name': input_json['name'],
+        'venue_address': input_json['location']
+    }
+
+    response = requests.request("POST", url, params=params)
+    return jsonify(response.json())
